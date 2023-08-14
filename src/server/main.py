0398792855cc5b48a635683061a841f925438fb1
ertosns <mohab-metwally@riseup.net>
2023-08-14 12:00:30 +0300

    update server with emojify&manualy tested

--- src/server/main.py
+++ src/server/main.py
@@ -4,31 +4,25 @@ from emojify2 import Emoji
 
 # __name__ is equal to app.py
 app = Flask(__name__)
-
-
+emoji_client = Emoji()
 @app.route("/", methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route("/chat", methods=['POST'])
+@app.route("/chat", methods=['GET', 'POST'])
 def chat():
-    #TODO get question
-    question = None
+    question = request.args['question']
     comp = Companion()
     response = comp.ask_question(question)
     return render_template("index.html", result=response)
 
 
-@app.route("/emojify", methods=['POST'])
+@app.route("/emojify", methods=['GET', 'POST'])
 def emojify():
-    #TODO
-    # 1) get statement
-    statement = None
-    # 2) query emojify
-    # 3) return emoji
-    emoji_client = Emoji()
-    emoji = emoji_client.emojify(statement)
-    return render_template("index.html", result=emoji)
+    statement = request.args['statement']
+    print('emojifying: {}'.format(statement))
+    ret = emoji_client.emojify(statement)
+    return render_template("index.html", result=ret)
 
 if __name__ == "__main__":
     app.run(debug=True)
