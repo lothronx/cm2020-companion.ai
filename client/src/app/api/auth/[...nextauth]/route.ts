7dcc5fe114f86df92b6720ea9436730a4546e53e
“lothron” <yuewuxd@gmail.com>
2023-08-16 21:12:45 +0800

    move bcrypt to the backend

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
-import * as bcrypt from "bcrypt";
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
@@ -12,13 +11,12 @@ const handler = NextAuth({
       },
 
       async authorize(credentials, req) {
-        const hashedPassword = credentials && (await bcrypt.hash(credentials.password, 10));
         const user = await fetch("http://localhost:5000/api/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             username: credentials?.username,
-            password: hashedPassword,
+            password: credentials?.password,
           }),
         }).then((res) => res.json());
 
