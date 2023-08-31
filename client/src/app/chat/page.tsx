70f7f48cba8b13fae12ec712d49c1c36adc3685d
“lothron” <yuewuxd@gmail.com>
2023-08-31 21:04:30 +0800

    update styling, disabled empty submit
    Now the chat page looks better with the input field in viewpoint.
    Users can no longer submit empty content to the backend.

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -28,7 +28,7 @@ export default function Chat() {
     register,
     handleSubmit,
     reset,
-    formState: { isSubmitting },
+    formState: { isSubmitting, isDirty, isValid },
   } = useForm<{ content: string }>();
 
   // useEffect
@@ -90,7 +90,7 @@ export default function Chat() {
       });
 
       const newMessages: Message[] = await response.json();
-      console.log(newMessages);
+
       setIsTyping(false);
 
       setMessages((messages) => [
@@ -110,7 +110,7 @@ export default function Chat() {
   };
 
   return (
-    <div className="h-screen md:container md:mx-auto shadow-lg rounded-lg">
+    <main className="h-screen flex flex-col md:container md:mx-auto shadow-lg rounded-lg">
       <header className="grid grid-cols-3 content-center bg-primary text-base-100 px-10 py-2">
         <Link href="/settings" className="justify-self-start grid content-center">
           <MdOutlineArrowBackIos className="text-3xl" />
@@ -121,60 +121,61 @@ export default function Chat() {
         </div>
       </header>
 
-      <main className="bg-base-100 h-5/6">
-        <section className="flex flex-col w-full h-full px-4 overflow-auto">
-          <header className="text-center text-xs text-primary my-3">
-            Companion.ai is powered by OpenAI. <br />
-            Everything AI says is not real.
-          </header>
+      <section className="grow bg-base-100 flex flex-col w-full h-full px-4 overflow-auto">
+        <header className="text-center text-xs text-primary my-3">
+          Companion.ai is powered by OpenAI. <br />
+          Everything AI says is not real.
+        </header>
 
-          {isLoading && (
-            <AiOutlineLoading3Quarters className="self-center text-2xl text-primary mt-10" />
-          )}
+        {isLoading && (
+          <AiOutlineLoading3Quarters className="self-center text-2xl text-primary mt-10" />
+        )}
 
-          <ul className="">
-            {messages.map((message) => (
-              <li key={message.id} className="w-full space-y-1.5 grid grid-cols-6 py-2">
-                <p
-                  className={`text-s break-words rounded-xl border border-dashed border-primary px-4 py-2 
+        <ul>
+          {messages.map((message) => (
+            <li key={message.id} className="w-full space-y-1.5 grid grid-cols-6 py-2">
+              <p
+                className={`text-s break-words rounded-xl border border-dashed border-primary px-4 py-2 
                  ${
                    message.role == "user"
                      ? "text-primary bg-base-100 col-start-2 col-end-7 justify-self-end"
                      : "bg-primary text-base-100 col-start-1 col-end-6 justify-self-start"
                  }`}>
-                  {message.content}
+                {message.content}
+              </p>
+              {message.emotion && (
+                <p className="text-xs text-primary text-right col-start-1 col-end-7 pb-1">
+                  Emotion detected: {message.emotion}
                 </p>
-                {message.emotion && (
-                  <p className="text-xs text-primary text-right col-start-1 col-end-7 pb-1">
-                    Emotion detected: {message.emotion}
-                  </p>
-                )}
-              </li>
-            ))}
-            <li className="text-slate-600 text-xs px-4 py-2 col-start-1 col-end-5 justify-self-start">
-              {isTyping && "AI is typing..."}
+              )}
             </li>
-          </ul>
-        </section>
-        <form
-          className="flex w-full h-10 py-2 px-4"
-          onSubmit={handleSubmit((data) => {
-            onSubmit(data);
-            reset();
-          })}>
-          <input
-            className="w-full h-full rounded px-4 py-6 text-sm"
-            {...register("content")}
-            type="text"
-            name="content"
-            id="content"
-            placeholder="Type your message here..."
-          />
-          <button className="px-4 py-2" type="submit" disabled={isSubmitting || isTyping}>
-            <MdArrowCircleUp className="text-4xl text-primary" />
-          </button>
-        </form>
-      </main>
-    </div>
+          ))}
+          <li className="text-slate-600 text-xs px-4 py-2 col-start-1 col-end-5 justify-self-start">
+            {isTyping && "AI is typing..."}
+          </li>
+        </ul>
+      </section>
+      <form
+        className="flex w-full py-2 px-4"
+        onSubmit={handleSubmit((data) => {
+          onSubmit(data);
+          reset();
+        })}>
+        <input
+          className="w-full h-full rounded px-4 py-6 text-sm"
+          {...register("content")}
+          type="text"
+          name="content"
+          id="content"
+          placeholder="Type your message here..."
+        />
+        <button
+          className="pl-4 py-2"
+          type="submit"
+          disabled={isSubmitting || isTyping || !isDirty || !isValid}>
+          <MdArrowCircleUp className="text-4xl text-primary" />
+        </button>
+      </form>
+    </main>
   );
 }
