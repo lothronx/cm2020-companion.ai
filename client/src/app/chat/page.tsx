d0653ba483495d8d181b408d8bed3c121814e5f9
alinaToonCC <136342857+alinaToonCC@users.noreply.github.com>
2023-09-10 23:31:10 +0800

    styling update

--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -132,17 +132,17 @@ export default function Chat() {
       <main className="h-screen flex flex-col md:container md:mx-auto shadow-lg rounded-lg">
         <header className="grid grid-cols-3 content-center bg-primary text-base-100 px-10 py-2 rounded-t-lg bg-gradient-to-r from-purple-500 to-pink-500">
           <Link href="/settings" className="justify-self-start grid content-center">
-            <MdOutlineArrowBackIos className="text-3xl" />
+            <MdOutlineArrowBackIos className="text-3xl  text-primary" />
           </Link>
           <div className="flex flex-col items-center">
-            <AiOutlineRobot className="text-5xl" />
-            <p className="mx-1">companion.ai</p>
+            <AiOutlineRobot className="text-5xl  text-primary" />
+            <p className="mx-1 font-medium  text-primary">companion.ai</p>
           </div>
         </header>
   
         <section className="grow bg-violet-100/30 w-full h-full px-4 overflow-auto">
           <div className="flex flex-col" ref={chatBox}>
-            <header className="text-center text-xs text-primary my-3">
+            <header className="text-center text-xs text-blue-900 my-3">
               Companion.ai is powered by OpenAI. <br />
               Everything AI says is not real.
             </header>
@@ -156,8 +156,8 @@ export default function Chat() {
                     className={`text-s break-words rounded-xl border border-dashed border-primary px-4 py-2
                      ${
                        message.role == "user"
-                         ? "text-primary bg-base-100 col-start-2 col-end-7 justify-self-end"
-                         : "bg-primary text-base-100 col-start-1 col-end-6 justify-self-start"
+                         ? "text-primary bg-base-100/70 col-start-2 col-end-7 justify-self-end"
+                         : "bg-violet-500/80 text-base-100 col-start-1 col-end-6 justify-self-start"
                      }`}>
                     {message.content}
                   </p>
