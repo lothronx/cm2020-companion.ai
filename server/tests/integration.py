74800ecd3b8a74dd25dd90c8e7bfa36fae71d2af
“lothron” <yuewuxd@gmail.com>
2023-09-16 22:00:22 +0800

    style: cleaning things up

--- server/tests/integration.py
+++ server/tests/integration.py
@@ -1,49 +1,54 @@
 from unittest import TestCase
 from main import *
 import ast
-from mongodbsetup import (insert_user, login, insert_ai_message, insert_user_message, get_messages)
+from mongodbsetup import (
+    insert_user,
+    login,
+    insert_ai_message,
+    insert_user_message,
+    get_messages,
+)
 from chatgptapi import CustomChatGPT
 
 
 class IntegrationTest(TestCase):
     def test_emoji(self):
         client = app.test_client()
-        res = ast.literal_eval(client.get('/api/emojify?statement=love is compassion').data.decode('utf-8'))[0]
-        emoji_res = res['emoji']
+        res = ast.literal_eval(
+            client.get("/api/emojify?statement=love is compassion").data.decode("utf-8")
+        )[0]
+        emoji_res = res["emoji"]
         self.assertEqual(int(emoji_res), 2)
 
     def test_new_user_success(self):
         client = app.test_client()
-        res = client.post('/api/login', json={
-            'email': 'name@email.net',
-            'password': 'pass'
-        })
+        res = client.post(
+            "/api/login", json={"email": "name@email.net", "password": "pass"}
+        )
         self.assertEqual(res.status_code, 200)
-        res = ast.literal_eval(res.data.decode('utf-8'))['status']
-        self.assertEqual(res, 'success')
-
+        res = ast.literal_eval(res.data.decode("utf-8"))["status"]
+        self.assertEqual(res, "success")
 
     def test_chat_history(self):
         client = app.test_client()
-        res = client.post('/api/login', json={
-            'email': 'name@email.net',
-            'password': 'pass'
-        })
+        res = client.post(
+            "/api/login", json={"email": "name@email.net", "password": "pass"}
+        )
         self.assertEqual(res.status_code, 200)
         data = res.data
-        print('data: {}'.format(data))
-        res = ast.literal_eval(data.decode('utf-8'))['status']
-        self.assertEqual(res, 'success')
-        user_id = ast.literal_eval(data.decode('utf-8'))['access_token']
+        print("data: {}".format(data))
+        res = ast.literal_eval(data.decode("utf-8"))["status"]
+        self.assertEqual(res, "success")
+        user_id = ast.literal_eval(data.decode("utf-8"))["access_token"]
         client = app.test_client()
-        res = client.get('/api/chat_history', json={
-            'email': 'name@email.net',
-            'password': 'pass'
-        }, headers={'Authorization': 'Bearer ' + str(user_id)})
+        res = client.get(
+            "/api/chat_history",
+            json={"email": "name@email.net", "password": "pass"},
+            headers={"Authorization": "Bearer " + str(user_id)},
+        )
         self.assertEqual(res.status_code, 200)
 
 
-
-if __name__ == '__main__':
-    main(module='test_module', exit=False)
+if __name__ == "__main__":
+    main(module="test_module", exit=False)
     unittest.main()
--- server/tests/main.py
+++ server/tests/main.py
@@ -1,48 +1,54 @@
 import unittest
 from unittest import main
 from emojify2 import Emoji
-from mongodbsetup import (insert_user, login, insert_ai_message, insert_user_message, get_messages)
+from mongodbsetup import (
+    insert_user,
+    login,
+    insert_ai_message,
+    insert_user_message,
+    get_messages,
+)
 from chatgptapi import CustomChatGPT
-
 
 class TestEmoji(unittest.TestCase):
     def test_love(self):
         emoji = Emoji()
-        analysis = emoji.emojify('i love you')
+        analysis = emoji.emojify("i love you")
         self.assertEqual(analysis, 0)
 
     def test_smile(self):
         emoji = Emoji()
-        analysis = emoji.emojify('love is compassion')
+        analysis = emoji.emojify("love is compassion")
         self.assertEqual(analysis, 2)
 
     def test_disappointment(self):
         emoji = Emoji()
-        analysis = emoji.emojify('was a bad day')
+        analysis = emoji.emojify("was a bad day")
         self.assertEqual(analysis, 3)
 
     def test_food(self):
         emoji = Emoji()
-        analysis = emoji.emojify('food was good')
+        analysis = emoji.emojify("food was good")
         self.assertEqual(analysis, 4)
 
     def test_baseball(self):
         emoji = Emoji()
-        analysis = emoji.emojify('i play baseball')
+        analysis = emoji.emojify("i play baseball")
         self.assertEqual(analysis, 1)
+
 
 class TestChatGpt(TestCase):
     def test_login_nouser(self):
-        user_email = 'abc@example.com'
-        password_hashed = 'abcd'
+        user_email = "abc@example.com"
+        password_hashed = "abcd"
         success, user_id, _ = login(user_email, password_hashed)
         self.assertEqual(success, False)
 
     def test_insert_newuser(self):
         # Insert a user into the users database
-        user_username = 'test'
-        user_email = 'test265rt@example.com'
-        password_hashed = 'testpassword'
+        user_username = "test"
+        user_email = "test265rt@example.com"
+        password_hashed = "testpassword"
         insert_user(user_username, user_email, password_hashed)
         success, user_id, _ = login(user_email, password_hashed)
         self.assertEqual(success, True)
@@ -52,9 +58,9 @@ class TestChatGpt(TestCase):
 
     def test_insert_user_insert_chat(self):
         # Insert a user into the users database
-        user_username = 'test'
-        user_email = 'test265rt@example.com'
-        password_hashed = 'testpassword'
+        user_username = "test"
+        user_email = "test265rt@example.com"
+        password_hashed = "testpassword"
         insert_user(user_username, user_email, password_hashed)
 
         # Login
@@ -62,20 +68,18 @@ class TestChatGpt(TestCase):
 
         self.assertEqual(success, True)
 
-
         user_id = "64de769fa8b4379871d54091"
         user_input = "How are you??"
         ai_response = CustomChatGPT(user_id, user_input)
 
-
-        insert_user_message(user_id,  user_input)
-        insert_ai_message(user_id,  ai_response)
-
+        insert_user_message(user_id, user_input)
+        insert_ai_message(user_id, ai_response)
 
         print("Fetching messages from the database...")
         messages = get_messages(user_id)
-        self.assertEqual(len(messages)>0, True)
+        self.assertEqual(len(messages) > 0, True)
 
-if __name__ == '__main__':
-    main(module='test_module', exit=False)
+
+if __name__ == "__main__":
+    main(module="test_module", exit=False)
     unittest.main()
